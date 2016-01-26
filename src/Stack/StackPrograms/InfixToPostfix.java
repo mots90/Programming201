fc08a544d548f822c7b9df5bcdfd14d7a0e0b268
Moheet Bhute <mbhute@asu.edu>
2016-01-25 23:09:09 -0600

    Infix to postfix conversion

---
+++ src/Stack/StackPrograms/InfixToPostfix.java
@@ -0,0 +1,107 @@
+package stack.stackPrograms;
+
+import stack.GenericStackUnchecked;
+
+/**
+ * Created by moheet-dev on 1/25/2016.
+ */
+public class InfixToPostfix {
+
+
+    private int charIndex = 0;
+
+    GenericStackUnchecked<Character> operatorStack;
+
+    InfixToPostfix() {
+        operatorStack = new GenericStackUnchecked<Character>();
+    }
+
+    public String convertInfixToPostfix (String infix) {
+        StringBuffer postfix = new StringBuffer();
+
+        while ( hasMoreCharacters(infix)) {
+
+            char ch = getNextCharacter(infix);
+
+            if (isOperand(ch)) {
+                postfix.append(ch);
+            }
+            else if (isOperator(ch)) {
+                    while(!operatorStack.isEmpty() && hasPrecedenceOverStackTop(ch)) {
+                        postfix.append(operatorStack.pop());
+                    }
+                    operatorStack.push(ch);
+            }
+            else {
+                throw new RuntimeException("please fuck off, invalid infix expression");
+            }
+        }
+
+        while (!operatorStack.isEmpty()) {
+            postfix.append(operatorStack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    private char getNextCharacter(String string) {
+        return (charIndex < string.length()) ? string.toCharArray()[charIndex++] : '#';
+    }
+
+    private boolean hasMoreCharacters(String string) {
+        return (charIndex < string.length() ) ? true : false;
+    }
+
+    private boolean isOperand(char ch) {
+        return ((ch >= 'A' && ch <='Z') || (ch >= 'a' && ch <='z')) ? true: false;
+    }
+
+    private boolean isOperator(char ch) {
+        return ((ch == '+') || (ch == '-') || (ch == '*') || (ch == '/')) ? true: false;
+    }
+
+    private boolean hasPrecedenceOverStackTop(char ch) {
+        if(operatorStack.isEmpty()) {
+            return false;
+        }
+        char stackTop = operatorStack.peek();
+        if (ch == '+' ) {
+            if (stackTop == '*' || stackTop == '/') {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        if (ch == '+' ) {
+            if (stackTop == '*' || stackTop == '/') {
+                return true;
+            }
+            else {
+                return true;
+            }
+        }
+        if (ch == '*' || ch == '/') {
+            if (stackTop == '+' || stackTop == '-') {
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        InfixToPostfix converter = new InfixToPostfix();
+
+        String infix = "A+B*C-D/E";
+
+        System.out.println("Postfix expression is " + converter.convertInfixToPostfix(infix));
+
+    }
+}
