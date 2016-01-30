d973719f68bd64a7de1ec6fb858123b01ef9388c
MB033889 <moheet.bhute@cerner.com>
2016-01-30 13:25:26 -0600

    abstracted out the common functionality of expression conversion like nextOperator, nextOperand, precedence

---
+++ src/Stack/StackPrograms/ExpressionConversion.java
@@ -0,0 +1,74 @@
+package stack.stackPrograms;
+
+import stack.GenericStackUnchecked;
+
+/**
+ * Created by mb033889 on 1/30/16.
+ */
+public abstract class ExpressionConversion {
+
+    private int charIndex = 0;
+
+    GenericStackUnchecked<Character> operatorStack;
+
+    char getNextCharacter(String string) {
+        return (charIndex < string.length()) ? string.toCharArray()[charIndex++] : '#';
+    }
+
+    boolean hasMoreCharacters(String string) {
+        return (charIndex < string.length() ) ? true : false;
+    }
+
+    boolean isOperand(char ch) {
+        return ((ch >= 'A' && ch <='Z') || (ch >= 'a' && ch <='z')) ? true: false;
+    }
+
+    boolean isOperator(char ch) {
+        return ((ch == '+') || (ch == '-') || (ch == '*') || (ch == '/') || ch == '(' || ch == ')') ? true: false;
+    }
+
+    boolean hasPrecedenceOverStackTop(char ch) {
+        if(operatorStack.isEmpty()) {
+            return false;
+        }
+
+        if(operatorStack.peek() == ')' || ch == ')') {
+            return true;
+        }
+        if (ch == '(' || operatorStack.peek() == '(') {
+            return false;
+        }
+
+
+
+        char stackTop = operatorStack.peek();
+        if (ch == '+' ) {
+            if (stackTop == '*' || stackTop == '/') {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        if (ch == '+' ) {
+            if (stackTop == '*' || stackTop == '/') {
+                return true;
+            }
+            else {
+                return true;
+            }
+        }
+        if (ch == '*' || ch == '/') {
+            if (stackTop == '+' || stackTop == '-') {
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+}
