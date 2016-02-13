bf34eaaae62bd70c6e19376b62027d03d59b71fe
MB033889 <moheet.bhute@cerner.com>
2016-02-12 22:28:54 -0600

    name change

---
+++ src/sortinglala/CountingSort.java
@@ -0,0 +1,70 @@
+package sortinglala;
+
+/**
+ * Created by moheet-dev on 1/20/2016.
+ */
+public class CountingSort {
+
+    public int[] countingSort(int [] array, int k) {
+
+        int inputLength = array.length;
+        int i;
+
+        // initialize counts of size with all zeros
+        int [] counts = new int[k];
+
+        //initialise the sorted array
+        int[] sorted = new int[inputLength];
+
+        // count the occurrences of each of elements and store in counts array
+        for(i=0; i<inputLength; i++) {
+            counts[array[i]] += 1;
+        }
+
+        // now that counts are known, we can modify counts to contain the number of preceding elements before i;
+        for (i=1; i < k; i++) {
+            counts[i] += counts[i-1];
+        }
+
+        // now position each element from array into their corresponding location
+        for (i=inputLength-1; i>=0; i--) {
+            sorted[--counts[array[i]]] = array[i];
+        }
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {4,2,5,1,3,2,4};
+        CountingSort sorter = new CountingSort();
+        for (int element : sorter.countingSort(array,6)){
+            System.out.print(element + "\t");
+        }
+
+    }
+}
+
+/*
+
+        int inputLength = array.length;
+
+        int i;
+        int[] counts = new int[k];
+        int[] sorted = new int[inputLength];
+
+        for (i = 0; i < inputLength; i++) {
+            counts[array[i]]++;
+        }
+
+        for (i = 1; i < k; i++) {
+            counts[i] = counts[i] + counts[i - 1];
+        }
+
+        for (i = inputLength - 1; i >= 0; i--){
+            sorted[--counts[array[i]]] = array[i];
+
+        }
+        return sorted;
+
+
+ */
---
+++ src/sortinglala/QuickSort.java
@@ -0,0 +1,56 @@
+package sortinglala;
+
+/**
+ * Created by moheet-dev on 1/20/2016.
+ */
+public class QuickSort {
+
+    public static void quickSort(int [] arr, int low, int high) {
+
+        if (low < high) {
+            int partition = partition(arr, low, high);
+            quickSort(arr,low,partition);
+            quickSort(arr,partition+1, high);
+        }
+    }
+
+    public static int partition(int [] arr, int low, int high) {
+
+        int pivot = arr[low];
+
+        int up = low;
+        int down = high;
+
+        while(true) {
+
+            while (up < high && arr[up] < pivot) {
+                up++;
+            }
+            while (down > low && arr[down] > pivot) {
+                down--;
+            }
+
+            if (up < down) {
+                int temp = arr[up];
+                arr[up] = arr[down];
+                arr[down] = temp;
+
+                up++;
+                down--;
+            }
+            else {
+                return down;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,6,2,1};
+        quickSort(arr, 0, arr.length -1);
+        for(int i:arr)  {
+            System.out.print("  "+i);
+        }
+    }
+
+}
